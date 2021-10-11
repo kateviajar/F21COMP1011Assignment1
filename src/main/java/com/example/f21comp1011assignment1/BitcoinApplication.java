5d965dadebbf9ae700d9fc6efbce16a4fd23380c
kateviajar <egbert0802@hotmail.com>
2021-10-10 23:55:48 -0700

    Add an icon to the title bar

--- src/main/java/com/example/f21comp1011assignment1/BitcoinApplication.java
+++ src/main/java/com/example/f21comp1011assignment1/BitcoinApplication.java
@@ -3,6 +3,7 @@ package com.example.f21comp1011assignment1;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ public class BitcoinApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(BitcoinApplication.class.getResource("bitcoin_table_view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Bitcoin");
+        stage.getIcons().add(new Image("file:./bitcoin-icon.png"));
         stage.setScene(scene);
         stage.show();
     }
